497355dc7ccf7dce0088827db11d443fdfd1d2ff
KharKhasino <ph1doxy@gmail.com>
2023-01-11 00:46:19 +0200

    first commit

---
+++ server/server.js
@@ -0,0 +1,53 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const config = new Configuration({
+  apikey: process.env.OPENAI_API_Key,
+});
+
+const openai = new OpenAIApi(config);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.status(200).send({
+    message: "Hello There!",
+    apikey:`${process.env.OPENAI_API_Key}`,
+
+  });
+
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0.4,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: error.message });
+  }
+});
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(process.env.OPENAI_API_Key);
+  console.log("Server is Running..");
+});
